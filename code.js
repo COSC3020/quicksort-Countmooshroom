838b7e6d632f40ad185da16d58ffb656608fe883
Countmooshroom <benjaminwatts314@gmail.com>
2023-10-04 16:37:29 -0600

    Wrote and iterative versio of quicksort

--- code.js
+++ code.js
@@ -1,3 +1,27 @@
 function quicksort(array) {
-    return array;
+    var arr = [array];
+    var finished = false;
+    while (!finished) {
+        let tempArr = []
+        finished = true;
+        for (const item of arr) {
+            if (item.length > 1) {
+
+                finished = false;
+                let pivot = item[0];
+                let left = [], right = [];
+                for (let j = 1; j < item.length; j++) {
+                    if (item[j] < pivot) { left.push(item[j]); }
+                    else { right.push(item[j]); }
+                }
+
+                if (left.length > 0) { tempArr.push(left); }
+                tempArr.push([item[0]])
+                if (right.length > 0) { tempArr.push(right); }
+            }
+            else { tempArr.push(item) }
+        }
+        arr = [...tempArr];
+    }
+    return arr.flat();
 }
